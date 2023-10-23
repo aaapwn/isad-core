17f6fb90244311a00cc68b61eb3152aa9dff3f24
aaapwn <armmykung2547@gmail.com>
2023-10-23 16:52:39 +0700

    add: docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:18
+WORKDIR /app
+COPY . .
+RUN npm install -g pnpm
+RUN pnpm install
+RUN pnpm db:generate
+RUN pnpm run build
+EXPOSE 3000
+CMD [ "pnpm run start" ]
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import app from "./src/app";
+import app from "./app";
 import functions from "firebase-functions";
-
+// import function from "firebase-functions";
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
-export const api = functions?.https?.onRequest(app);
+export const server = functions?.https?.onRequest(app);
